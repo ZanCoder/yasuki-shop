a2e9b471953692dc9d439d8a790df156b776ef6a
tuankiets <luctuankietkg@gmail.com>
2023-07-17 22:16:20 +0700

    setup layout FE

--- src/main/java/com/poly/Yasuki/controller/HomeController.java
+++ src/main/java/com/poly/Yasuki/controller/HomeController.java
@@ -2,13 +2,44 @@ package com.poly.Yasuki.controller;
 
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class HomeController {
 
-    @GetMapping({"/", "index"})
-    public String viewHomePage(){
+    @GetMapping({"/", "/index"})
+    public String viewHomePage(Model model){
         return "user/index";
+    }
+    @GetMapping("/list-product")
+    public String viewListProductPage(Model model){
+        return "user/listProduct";
+    }
+    @GetMapping("/cart")
+    public String viewCartPage(Model model){
+        return "user/cart";
+    }
+    @GetMapping("/news")
+    public String viewNewsPage(Model model){
+        return "user/news";
+    }
+
+    @GetMapping("/contact")
+    public String viewContactPage(Model model){
+        return "user/contact";
+    }
+
+    @GetMapping("/payment")
+    public String viewPaymentPage(Model model){
+        return "user/pay";
+    }
+    @GetMapping("/product")
+    public String viewProductPage(Model model){
+        return "user/product";
+    }
+    @GetMapping("/profile")
+    public String viewProfilePage(Model model){
+        return "user/profile";
     }
 }
