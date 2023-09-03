8129975d4a7f13154b7d252c356d03f353980b98
tuankiets <luctuankietkg@gmail.com>
2023-09-03 19:17:58 +0700

    update interface

--- src/main/resources/static/assets/user/js/commonscript.js
+++ src/main/resources/static/assets/user/js/commonscript.js
@@ -22,10 +22,10 @@ const panes = document.querySelectorAll(".tab-pane");
 
 const tabActive = document.querySelector(".tab-item.active");
 const line = document.querySelector(".tabs .line");
-if (line) {
+/*if (line) {
     line.style.left = tabActive.offsetLeft + "px";
     line.style.width = tabActive.offsetWidth + "px";
-}
+}*/
 
 
 tabs.forEach((tab, index) => {
@@ -34,8 +34,8 @@ tabs.forEach((tab, index) => {
         document.querySelector(".tab-item.active").classList.remove("active");
         document.querySelector(".tab-pane.active").classList.remove("active");
 
-        line.style.left = this.offsetLeft + "px";
-        line.style.width = this.offsetWidth + "px";
+        /*line.style.left = this.offsetLeft + "px";
+        line.style.width = this.offsetWidth + "px";*/
 
         this.classList.add("active");
         pane.classList.add("active");
